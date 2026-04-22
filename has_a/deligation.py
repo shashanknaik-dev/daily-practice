d173f3f64ba41a750e8b86460cdf2b58d9f87477
Shashank Naik <shashanknaik625@gmail.com>
2026-04-22 18:37:04 +0530

    deligation simple ex practice

--- has_a/deligation.py
+++ has_a/deligation.py
@@ -14,3 +14,21 @@ print(c1.min)
 print(c1.max)
 c1.R.turnon(1)
 c1.R.turnon(0)
+
+# repeat
+class Radio:
+    def turnon(self,x):
+        if x==1:
+            print("radio is on")
+        else:
+            print("radio is off")
+class Car1:
+    def __init__(self,min,max):
+        self.min=min
+        self.max=max
+        self.R=Radio()
+c2=Car1(60,100)
+print(c2.min)
+print(c2.max)
+c2.R.turnon(-1)
+c2.R.turnon(1)
