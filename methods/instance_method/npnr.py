f9fd279553295ba6c2600edb1c4caa384538ec15
Shashank Naik <shashanknaik625@gmail.com>
2026-04-24 20:27:22 +0530

    no parameter no return value

---
+++ methods/instance_method/npnr.py
@@ -0,0 +1,13 @@
+# no parameter no return value
+class Calci:
+    def __init__(self):
+        self.numbers=10
+        self.color="black"
+    def add(self):
+        a=10
+        b=20
+        print(a+b)
+c=Calci()
+print(c.numbers)
+print(c.color)
+c.add()
